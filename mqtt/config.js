0e09b68b51e5c3d54b1210432dedc966a08ba166
hosyhau97 <hackingneo97@gmail.com>
2018-10-25 00:47:51 +0700

    Combine all sensor together

--- mqtt/config.js
+++ mqtt/config.js
@@ -3,7 +3,7 @@ const password = "27XuuEgTYxKt";
 
 module.exports.mqtt = {
     CLOUDMQTT_URL: `mqtt://${userName}:${password}@m15.cloudmqtt.com:19067`,
-    TOPIC_SENSOR: ["light_sensor", "temp_sensor", "humidity_sensor", "air_sensor", "soil_sensor"],
+    TOPIC_SENSOR: ["device/sensor"],
     TOPIC_ENGINE:
         ["control/water" ,"control/light","control/fan",'light_down','light_up','humidity_down',
         'humdity_up','temp_down','temp_up','temp_down', "control/roof"]
